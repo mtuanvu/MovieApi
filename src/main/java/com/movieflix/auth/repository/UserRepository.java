3ff296f53325ecc69b8bc149e05956e3cf138ae4
mtuanvu <143334278+mtuanvu@users.noreply.github.com>
2024-10-28 02:41:46 +0700

    up forgot password

--- src/main/java/com/movieflix/auth/repository/UserRepository.java
+++ src/main/java/com/movieflix/auth/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package com.movieflix.auth.repository;
 
 import com.movieflix.auth.entity.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -9,4 +12,9 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUserName(String userName);
 
     Optional<User> findByEmail(String userName);
+
+    @Transactional
+    @Modifying
+    @Query("update User u set u.password = ?2 where u.email = ?1")
+    void updatePassword(String email, String password);
 }
